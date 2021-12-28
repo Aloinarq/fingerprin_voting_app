7d39a03144955cf587a69ae6a43af6fb348058bc
Aloinarq <41049945+Aloinarq@users.noreply.github.com>
2021-12-28 18:02:49 +0200

    #register-login-home: Huge changes, implemented providers and models, improved data access from database. Finished homescreen(polls screen), candidate screen and details screen.

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,8 +1,9 @@
-import 'package:fingerprin_voting_app/services/check_things.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:fingerprin_voting_app/services/auth.dart';
+import 'package:fingerprin_voting_app/services/check_things.dart';
 import 'package:fingerprin_voting_app/services/database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
 
@@ -41,7 +42,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -50,171 +50,192 @@ class _RegisterScreenState extends State<RegisterScreen> {
         return MediaQuery(
           data: const MediaQueryData(),
           child: Scaffold(
-          appBar: AppBar(
-            title: Text("Register screen"),
-          ),
-          key: Key('RegisterScreen'),
-          body: Container(
-            height: double.infinity,
-            margin: EdgeInsets.all(24.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Type in your full name",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  SizedBox(height: 8),
-                  TextField(
-                      decoration: InputDecoration(border: OutlineInputBorder()),
+            appBar: AppBar(
+              title: Text("Register screen"),
+            ),
+            key: Key('RegisterScreen'),
+            body: Container(
+              height: double.infinity,
+              margin: EdgeInsets.all(24.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Type in your full name",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    SizedBox(height: 8),
+                    TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                        onChanged: (value) {
+                          setState(() {
+                            _fullName = value;
+                          });
+                        }),
+                    SizedBox(height: 16),
+                    Text("Type in your address",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    SizedBox(height: 8),
+                    TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                        onChanged: (value) {
+                          setState(() {
+                            _address = value;
+                          });
+                        }),
+                    SizedBox(height: 16),
+                    Text("Type in your CNP",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    SizedBox(height: 8),
+                    TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                        onChanged: (value) {
+                          setState(() {
+                            _cnp = value;
+                          });
+                        }),
+                    SizedBox(height: 16),
+                    Text("Type in your e-mail address",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    SizedBox(height: 8),
+                    TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                        onChanged: (value) {
+                          setState(() {
+                            _email = value;
+                          });
+                        }),
+                    SizedBox(height: 16),
+                    Text("Create password",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    Text(
+                      "(minimum 8 characters, containing an uppercase letter and a number)",
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    SizedBox(height: 8),
+                    TextField(
+                      obscureText: _obscureText,
                       onChanged: (value) {
                         setState(() {
-                          _fullName = value;
+                          _password = value;
                         });
-                      }),
-                  SizedBox(height: 16),Text("Type in your address",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  SizedBox(height: 8),
-                  TextField(
+                      },
                       decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                    SizedBox(height: 16),
+                    Text("Confirm password",
+                        style: Theme.of(context).textTheme.subtitle2),
+                    SizedBox(height: 8),
+                    TextField(
+                      obscureText: _obscureText,
                       onChanged: (value) {
                         setState(() {
-                          _address = value;
+                          _passwordAgain = value;
                         });
-                      }),
-                  SizedBox(height: 16),
-                  Text("Type in your CNP",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  SizedBox(height: 8),
-                  TextField(
-                      decoration: InputDecoration(border: OutlineInputBorder()),
-                      onChanged: (value) {
-                        setState(() {
-                          _cnp = value;
-                        });
-                      }),
-                  SizedBox(height: 16),
-                  Text("Type in your e-mail address",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  SizedBox(height: 8),
-                  TextField(
-                      decoration: InputDecoration(border: OutlineInputBorder()),
-                      onChanged: (value) {
-                        setState(() {
-                          _email = value;
-                        });
-                      }),
-                  SizedBox(height: 16),
-                  Text("Create password",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  Text("(minimum 8 characters, containing an uppercase letter and a number)",
-                    style: Theme.of(context).textTheme.caption,
-                  ),
-                  SizedBox(height: 8),
-                  TextField(
-                    obscureText: _obscureText,
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value;
-                      });
-                    },
-                    decoration: InputDecoration(border: OutlineInputBorder()),
-                  ),
-                  SizedBox(height: 16),
-                  Text("Confirm password",
-                      style: Theme.of(context).textTheme.subtitle2),
-                  SizedBox(height: 8),
-                  TextField(
-                    obscureText: _obscureText,
-                    onChanged: (value) {
-                      setState(() {
-                        _passwordAgain = value;
-                      });
-                    },
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                  TextButton(
-                    onPressed: _togglePasswordVisibility,
-                    child: Row(children: [
-                      Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
-                      SizedBox(width: 8),
-                      Text(_obscureText
-                          ? "Show password"
-                          : "Hide password")
-                    ]),
-                  ),
-                  Center(
-                    child: SizedBox(
-                      height: 56,
-                      width: double.maxFinite,
-                      child: ElevatedButton(
-                        onPressed: (_fullName.isEmpty == true || _address.isEmpty == true || _cnp.isEmpty == true || _email.isEmpty == true
-                            || _validateEmail(_email) == false || !(_password == _passwordAgain) || _validatePassword(_password) == false)
-                            ? null
-                            : () async {
-                          bool _isConnected = await CheckThings().internet("www.firebase.com");
-                          bool _isCallSuccessful = await AuthService().registerEmailPassword(_email, _password);
-                          if(_isConnected == true) {
-                            if (_isCallSuccessful == true) {
-                              await Database().addUser(
-                                  _fullName, _address, _cnp);
-                              Navigator.push(context, MaterialPageRoute(
-                                  builder: (context) => const HomeScreen()));
-                              //Navigator.pushReplacementNamed(context, "/polls");
-                              final snackBar = SnackBar(
-                                backgroundColor: Colors.blueAccent,
-                                content: Row(
-                                  children: [
-                                    const Icon(Icons.check_circle_rounded),
-                                    const SizedBox(width: 8),
-                                    Expanded(
-                                        child: Text("Success", style: Theme
-                                            .of(context)
-                                            .textTheme
-                                            .bodyText1)),
-                                  ],
-                                ),
-                              );
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  snackBar);
-                            } else {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const AlertDialog(
-                                    title: Text("This email is already in use!"),
-                                  );
+                      },
+                      decoration:
+                          const InputDecoration(border: OutlineInputBorder()),
+                    ),
+                    TextButton(
+                      onPressed: _togglePasswordVisibility,
+                      child: Row(children: [
+                        Icon(_obscureText
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        SizedBox(width: 8),
+                        Text(_obscureText ? "Show password" : "Hide password")
+                      ]),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        height: 56,
+                        width: double.maxFinite,
+                        child: ElevatedButton(
+                          onPressed: (_fullName.isEmpty == true ||
+                                  _address.isEmpty == true ||
+                                  _cnp.isEmpty == true ||
+                                  _email.isEmpty == true ||
+                                  _validateEmail(_email) == false ||
+                                  !(_password == _passwordAgain) ||
+                                  _validatePassword(_password) == false)
+                              ? null
+                              : () async {
+                                  bool _isConnected = await CheckThings()
+                                      .internet("www.firebase.com");
+                                  bool _isCallSuccessful = await AuthService()
+                                      .registerEmailPassword(_email, _password);
+                                  if (_isConnected == true) {
+                                    if (_isCallSuccessful == true) {
+                                      await Database()
+                                          .addUser(_fullName, _address, _cnp);
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const HomeScreen()));
+                                      //Navigator.pushReplacementNamed(context, "/polls");
+                                      final snackBar = SnackBar(
+                                        backgroundColor: Colors.blueAccent,
+                                        content: Row(
+                                          children: [
+                                            const Icon(
+                                                Icons.check_circle_rounded),
+                                            const SizedBox(width: 8),
+                                            Expanded(
+                                                child: Text("Success",
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .bodyText1)),
+                                          ],
+                                        ),
+                                      );
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(snackBar);
+                                    } else {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return const AlertDialog(
+                                            title: Text(
+                                                "This email is already in use!"),
+                                          );
+                                        },
+                                      );
+                                    }
+                                  } else {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return const AlertDialog(
+                                          title: Text(
+                                              "You are not connected to the internet!"),
+                                        );
+                                      },
+                                    );
+                                  }
                                 },
-                              );
-                            }
-                          } else {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return const AlertDialog(
-                                  title: Text("You are not connected to the internet!"),
-                                );
-                              },
-                            );
-                          }
-                        },
-                        child: const Text(
-                          "Register",
+                          child: const Text(
+                            "Register",
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Already have an account? Log in!")),
-                  )
-                ],
+                    Container(
+                      alignment: Alignment.center,
+                      child: TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child:
+                              const Text("Already have an account? Log in!")),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
           ),
         );
       },
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,21 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class Database {
   final FirebaseFirestore fireStore = FirebaseFirestore.instance;
 
   var db = FirebaseFirestore.instance;
   CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-
   Future<void> addUser(String fullName, String address, String cnp) async {
-    return users.add({
-      "address" : address,
-      "cnp" : cnp,
-      "fullName" : fullName
-    }).then((value) => print("User added"))
-      .catchError((onError) => print(onError.toString()));
+    return users
+        .add({"address": address, "cnp": cnp, "fullName": fullName})
+        .then((value) => print("User added"))
+        .catchError((onError) => print(onError.toString()));
   }
-
-
 }
